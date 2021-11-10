e5cc4f610166ba55ce571bdc7d555e3626331c09
Aaron Phillips <abphillips1@buffs.wtamu.edu>
2021-11-10 17:27:17 -0600

    Rough Draft

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp1.Pages
+{
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
+
+//above code was changed to test denying anonymous access to the home pages.
+
+///*****without testing identity use below code
+
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.Extensions.Logging;
+
+///namespace WebApp1.Pages;
+
+//public class PrivacyModel : PageModel
+//{
+   // private readonly ILogger<PrivacyModel> _logger;
+
+    //public PrivacyModel(ILogger<PrivacyModel> logger)
+    //{
+       // _logger = logger;
+    //}
+
+    //public void OnGet()
+    //{
+   // }
+//}
